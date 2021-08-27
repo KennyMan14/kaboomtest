501127bfdf1eef08fa2c8da921b9c16e13b23037
Kenneth Herrera <69918528+kenneyher@users.noreply.github.com>
2021-08-27 16:40:44 -0600

    added sprites

--- rpg(knight quest) game/main.js
+++ rpg(knight quest) game/main.js
@@ -1,21 +1,51 @@
+let keyCollected = play("collected");
+
 scene("main", (levelInx) => {
   const SPEED = 80;
+  const SCALE = 1;
 
   const npc = {
     "f": {
       sprite: "npc1",
       msg: "the king must be furious with me... I LOST HIS ANIMALS!!",
       name: "farmer",
+      textsize: 10,
     },
     "k": {
       sprite: "npc2",
       msg: "i can not have anything! I always lose everything!",
       name: "king",
+      textsize: 10,
     },
     "q": {
       sprite: "npc3",
       msg: "oh no! someone stole my diamond necklace, can you help me?",
       name: "queen",
+      textsize: 10,
+    },
+    "e": {
+      sprite: "npc4",
+      msg: "so,you're the new guy,huh? This castle has a secret, it has a hidden room go to the armery, but be careful with traps and puzzles",
+      name: "elite",
+      textsize: 6,
+    },
+    "s": {
+      sprite: "npc5",
+      msg: "someone released the ghosts captured by the mage, they terrify me",
+      name: "servant",
+      textsize: 8,
+    },
+    "m": {
+      sprite: "npc6",
+      msg: "For the chains of Utama! Who's the bandido who released my ghosts??!! Please be careful, they're dangerous!",
+      name: "mage",
+      textsize: 6,
+    },
+    "a": {
+      sprite: "npc7",
+      msg: "Agh!These ghosts are so annoying, they interrupted me and I had to escape from the armory be careful boy i activated the traps",
+      name: "armorer",
+      textsize: 5,
     },
   };
 
@@ -46,8 +76,72 @@ scene("main", (levelInx) => {
     "=        =",
     "=$       =",
     "==========",
-  ]
-  ];
+  ],[
+    "=================",
+    "=e        ^     =",
+    "=         ^   $ =",
+    "=     ^   ^     =",
+    "=@    ^   ^     =",
+    "=     ^         =",
+    "=     ^         =",
+    "========|========",
+  ], [
+    "==========",
+    "=   @    =",
+    "=        =",
+    "=  ^^^^^^=",
+    "|        =",
+    "=        =",
+    "=^^^^^^ $=",
+    "==========",
+  ], [
+    "==========",
+    "=    *   =",
+    "=  *     =",
+    "=        =",
+    "|    *  @=",
+    "=        =",
+    "= *     s=",
+    "=    *   =",
+    "==========",
+  ], [
+    "================",
+    "=m            @=",
+    "=              =",
+    "=              =",
+    "= *    *     * =",
+    "=              =",
+    "=    *    *    =",
+    "=^^^^^^^^^^^   =",
+    "= *   *    *   =",
+    "=              =",
+    "=              =",
+    "=^  ^   ^   ^  =",
+    "=              =",
+    "=              =",
+    "========|=======",    
+  ],[
+    "==============",
+    "=a    @      =",
+    "=            =",
+    "=    *       =",
+    "|            =",
+    "=       *   *=",
+    "=   *        =",
+    "=       $    =",
+    "= *          =",
+    "==============",
+  ], [
+    "===================",
+    "=      ^  *   *   =",
+    "=                 =",
+    "=@     ^  $   *   |",
+    "=                 =",
+    "=         *       =",
+    "=      ^      *   =",
+    "===================",
+  ],
+];
 
   const map = addLevel(levels[levelInx], {
     width: 20,
@@ -71,8 +165,18 @@ scene("main", (levelInx) => {
     ],
     "$": [
       sprite("key"),
-      area(),
+      area(vec2(5), vec2(5)),
       "key",
+    ],
+    "^": [
+      sprite("lavafloor"),
+      area(),
+      "hazard",
+    ],
+    "*": [
+      sprite("ghost"),
+      area(vec2(8), vec2(8)),
+      "hazard",
     ],
     any(ch) {
       const char = npc[ch];
@@ -85,6 +189,7 @@ scene("main", (levelInx) => {
           {
             msg: char.msg,
             name: char.name,
+            textsize: char.textsize,
           },
         ];
       }
@@ -94,10 +199,10 @@ scene("main", (levelInx) => {
   let talking = null;
   let hasKey = false;
 
-  function talk(msg, char) {
+  function talk(msg, char, size) {
     talking = add([
-      text(char + ": " + msg, 10),
-      color(0, 0, 0),
+      text(char + ": " + msg, size),
+      color(1, 1, 1),
     ])
   }
 
@@ -105,28 +210,34 @@ scene("main", (levelInx) => {
 
   player.overlaps("door", () => {
     // if(levelInx  2){
-      if(levelInx != 2){
-        go("main", levelInx + 1);
-      }else {
+      if(levelInx == 2 || levelInx == 3 || levelInx == 4 || levelInx == 7 || levelInx == 8){
         if(hasKey == true){
           go("main", levelInx + 1);
         }else {
-          talk("get the key first", "clue")
+          talk("get the key first", "clue", 10)
         }
+      }else {
+        go("main", levelInx + 1);
       }
     // }
   });
 
+  player.collides("hazard", () => {
+    go("gameover", levelInx);
+  })
+
   player.overlaps("npc", (ch) => {
-    talk(ch.msg, ch.name);
+    talk(ch.msg, ch.name, ch.textsize);
   });
 
-  if(levelInx == 2){
+  // if(levelInx == 2 || levelInx == 3){
     player.overlaps("key", (key) => {
+      play("collected");
 	  	destroy(key);
+      // keyCollected.play();
 	  	hasKey = true;
 	  });
-  }
+  // }
 
   const dirs = {
     "left": vec2(-1, 0),
@@ -150,9 +261,29 @@ scene("main", (levelInx) => {
     });
   }
 
+  // keyPress("right", () => {
+  //   player.scale.x = SCALE;
+  // });
+
+  // keyPress("left", () => {
+  //   player.scale.x = -SCALE;
+  // })
+
   player.action(() => {
     player.pushOutAll();
   });
+
+});
+
+scene("gameover", (lvlIndex) => {
+  add([
+    text("you died, level reached: " + (lvlIndex+1)),
+    pos(width()/2, height()/2),
+    origin("center"),
+  ])
+  keyPress("space", () => go("main", lvlIndex));
+	mouseClick(() => go("main", lvlIndex));
 });
 
 go("main", 0);
+focus();
